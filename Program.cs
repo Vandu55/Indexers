d0ee99a6b425548293b5c6def45d47c71b2d30d0
Ankit-Kumar4 <113576375+Ankit-Kumar4@users.noreply.github.com>
2023-09-25 22:14:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using Indexers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    internal class Employee
+    {
+        private int[] Age = new int[10];
+
+        public int this[int index]
+        {
+            set
+            {
+                if (index >= 0 && index < Age.Length)
+                {
+                    if (value > 0)
+                    {
+                        Age[index] = value;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid  value:");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid index");
+                }
+            }
+            get
+            {
+                return Age[index];
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+        emp[0] = -8;
+        Console.WriteLine(emp[3]);
+        Console.ReadLine();
+    }
+}
+
+
+  
